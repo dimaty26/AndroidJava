6e3fde4fb86a87bb863d69eaddcd6b4c243d596c
dimaty26 <dimaty26rus@mail.ru>
2021-12-04 19:23:55 +0300

    HW-8 Added drawer menu

--- AndroidHW6/app/src/main/java/ru/geekbrains/androidhw6/fragments/NotesMainFragment.java
+++ AndroidHW6/app/src/main/java/ru/geekbrains/androidhw6/fragments/NotesMainFragment.java
@@ -2,11 +2,13 @@ package ru.geekbrains.androidhw6.fragments;
 
 import android.os.Bundle;
 
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import com.google.android.material.button.MaterialButton;
 
