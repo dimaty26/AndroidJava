cdd65059f146c52e843b616fdc2ce867121dc03b
dimaty26 <dimaty26rus@mail.ru>
2021-12-05 17:00:03 +0300

    Homework8and9 Modified toolbar to work correctly. Added alert dialog on exit

--- AndroidHW9and10/app/src/main/java/ru/geekbrains/androidhw9and10/MainActivity.java
+++ AndroidHW9and10/app/src/main/java/ru/geekbrains/androidhw9and10/MainActivity.java
@@ -1,8 +1,16 @@
 package ru.geekbrains.androidhw9and10;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+import ru.geekbrains.androidhw9and10.fragments.AboutFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.custom_toolbar);
+        setSupportActionBar(toolbar);
+        initDrawerMenu(toolbar);
+    }
+
+    private void initDrawerMenu(Toolbar toolbar) {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.action_about:
+                    openAboutFragment();
+                    drawerLayout.closeDrawers();
+                    return true;
+                case R.id.action_exit:
+                    openAlertExitDialog();
+                    return true;
+            }
+            return false;
+        });
+    }
+
+    private void openAlertExitDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(R.string.exit);
+        builder.setMessage("Do you really want to exit?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", (dialogInterface, i) -> finish());
+        builder.setNegativeButton("No", (dialogInterface, i) -> dialogInterface.dismiss());
+        builder.create().show();
+    }
+
+    private void openAboutFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack("")
+                .replace(R.id.fragment_container, new AboutFragment())
+                .commit();
     }
 }
